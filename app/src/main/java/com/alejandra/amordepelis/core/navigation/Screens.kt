f6400b3e3be4218c7d01bb8923064c6e97417a25
Alejandra-0805 <alejandraclemente48@gmail.com>
2026-04-04 21:07:00 -0600

    Navigation Home

--- app/src/main/java/com/alejandra/amordepelis/core/navigation/Screens.kt
+++ app/src/main/java/com/alejandra/amordepelis/core/navigation/Screens.kt
@@ -1,2 +1,12 @@
 package com.alejandra.amordepelis.core.navigation
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Login
+
+@Serializable
+object Register
+
+@Serializable
+object Home
