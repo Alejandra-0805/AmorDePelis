3172fd412758b0dfa502ff1125792c0acf00d9f3
ArgeliaGonzalez <niniveargeliaperezgonzalez@gmail.com>
2026-04-15 09:10:01 -0600

    hardwRare

--- app/src/main/java/com/alejandra/amordepelis/core/hardware/domain/Camera.kt
+++ app/src/main/java/com/alejandra/amordepelis/core/hardware/domain/Camera.kt
@@ -1,4 +1,20 @@
 package com.alejandra.amordepelis.core.hardware.domain
 
-class Camera {
+import android.net.Uri
+import androidx.camera.core.ImageCapture
+import androidx.camera.view.PreviewView
+import androidx.lifecycle.LifecycleOwner
+
+interface Camera {
+    fun hasCamera(): Boolean
+    fun startPreview(
+        lifecycleOwner: LifecycleOwner,
+        previewView: PreviewView,
+        onCameraReady: () -> Unit = {}
+    )
+    fun takePhoto(
+        onPhotoCaptured: (Uri) -> Unit,
+        onError: (Exception) -> Unit
+    )
+    fun stopCamera()
 }
